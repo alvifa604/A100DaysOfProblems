6c86ec49577804ad9d6f160a8e37f00c0deaefa7
Alberto Villalobos <alvifa95@outlook.com>
2024-03-01 11:41:54 -0600

    TwoSum

---
+++ ProblemsTests/Day1Tests/TwoSumTests.cs
@@ -0,0 +1,17 @@
+﻿using Problems;
+
+namespace ProblemsTests;
+
+public class TwoSumTests
+{
+    [Theory]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+    [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+    [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+    [InlineData(new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11, new int[] { 5, 11 })]
+    public void SolutionTest(int[] nums, int target, int[] expected)
+    {
+        int[] result = TwoSum.Solve(nums, target);
+        Assert.Equal(expected, result);
+    }
+}
