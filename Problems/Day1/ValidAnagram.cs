2a1436e9c749b5cf009cb5484ddf837b20a29006
Alberto Villalobos <alvifa95@outlook.com>
2024-03-01 11:24:26 -0600

    Valid anagram

---
+++ Problems/Day1/ValidAnagram.cs
@@ -0,0 +1,33 @@
+﻿namespace Problems;
+
+/*
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+    typically using all the original letters exactly once.
+
+    Example 1:
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+    
+    Example 2:
+    Input: s = "rat", t = "car"
+    Output: false
+*/
+public class ValidAnagram
+{
+    // Space: O(n)
+    // Time: O(n)
+    public static bool IsAnagram(string s, string t)
+    {
+        // No point in continue if strings have different lengths
+        if (s.Length != t.Length) return false;
+
+        Dictionary<char, int> letterCount = [];
+        for (int i = 0; i < s.Length; i++)
+        {
+            letterCount[s[i]] = letterCount.GetValueOrDefault(s[i], 0) + 1;
+            letterCount[t[i]] = letterCount.GetValueOrDefault(t[i], 0) - 1;
+        }
+
+        return letterCount.All(kv => kv.Value == 0);
+    }
+}
---
+++ ProblemsTests/Day1Tests/ValidAnagramTests.cs
@@ -0,0 +1,17 @@
+﻿using Problems;
+
+namespace ProblemsTests;
+
+public class ValidAnagramTests
+{
+    [Theory]
+    [InlineData("anagram", "nagaram", true)]
+    [InlineData("rat", "car", false)]
+    [InlineData("a", "ab", false)]
+    [InlineData("ab", "a", false)]
+    public void IsAnagramTest(string s, string t, bool expected)
+    {
+        var result = ValidAnagram.IsAnagram(s, t);
+        Assert.Equal(expected, result);
+    }
+}
